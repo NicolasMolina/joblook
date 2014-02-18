4fc10dde3745f451363bd2121e2566b0dac45eb4
Nicolas Molina <nicolas.molinabrizuela@gmail.com>
2014-02-18 11:26:33 +0200

    settingup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Joblook::Application.config.secret_key_base = 'ad7c0c73d69f1cb7baf706020a3d6f1e6288c49627626f8eb1e127e042962cff4eb12557479d20a52a8d911a44a831641d6f12bfe8905ad91f5981d60a065248'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Joblook::Application.config.session_store :cookie_store, key: '_joblook_session'
